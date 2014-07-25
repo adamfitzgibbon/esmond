751eb9ecd7e7750c8166f60a9757977a171f4f84
afitz <afitz@hcc-pki-ps02.localdomain>
2014-07-25 12:05:41 -0500

    Retry

--- pki02.py
+++
@@ -1,7 +0,0 @@
-from esmonduploader import *
-
-caller = FilterArgs(verbose=True,start=-3600,end=0,source='198.129.254.30',tool_name='bwcrtl/iperf3',connect='http://hcc-pki-ps02.unl.edu:8000/')
-
-conn = caller.apiConn()
-
-caller.getData(conn)
